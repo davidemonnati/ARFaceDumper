c5cbd119ee6054175f3e4f270b950f4f30805817
Davide Monnati <davide.monnati@gmail.com>
2026-03-01 23:49:11 +0100

    create Save depth button

--- ARFaceDumper/ContentView.swift
+++ ARFaceDumper/ContentView.swift
@@ -27,6 +27,12 @@ struct ContentView: View {
                 Text(arSceneView.status)
                     .font(.system(.caption, design: .monospaced))
             }
+            
+            HStack {
+                Button("Save depth", systemImage: "square.and.arrow.up") {
+                    arSceneView.captureDepthImage()
+                }
+            }
         }
     }
 }
@@ -42,6 +48,8 @@ struct ARFaceSceneView: UIViewRepresentable {
         view.session.delegate = arSceneView
         view.session.run(config)
         
+        arSceneView.arView = view
+        
         return view
     }
     
@@ -51,7 +59,8 @@ struct ARFaceSceneView: UIViewRepresentable {
 class ARSceneView: NSObject, ObservableObject, ARSessionDelegate {
     @Published var status: String = "Waiting for the face..."
     
-    private var frameCount = 0;
+    private var frameCount = 0
+    var arView: ARView?
     
     func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
         for anchor in anchors {
@@ -94,6 +103,43 @@ Eyes R: \(eyeBlinkR>0.3 ? "CLOSED" : "OPEN") \(String(format: "%.2f", eyeBlinkR)
 Smile: \(String(format: "%.0f%%", smile * 100))
 Tongue: \(tongue > 0.5 ? "OUT" : "IN")
 """
+    }
+    
+    func captureDepthImage() {
+        guard let frame = arView?.session.currentFrame else {
+            print("arView is nil")
+            return
+        }
+        
+        guard let depthData = frame.capturedDepthData else {
+            print("Depth data not captured in the current frame")
+            return
+        }
+        
+        let depthMap = depthData.depthDataMap
+        
+        saveDepthImage(depthMap: depthMap)
+    }
+    
+    func saveDepthImage(depthMap: CVPixelBuffer) {
+        let ciImage = CIImage(cvPixelBuffer: depthMap)
+        let context = CIContext()
+        
+        // Normalize depth for visualization (depth is in meters as Float32)
+        guard let cgImage = context.createCGImage(ciImage, from: ciImage.extent) else { return }
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        print(uiImage)
+        
+        UIImageWriteToSavedPhotosAlbum(uiImage, self, #selector(imageSaved(_:didFinishSavingWithError:contextInfo:)), nil)
+    }
+    
+    @objc func imageSaved(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeMutableRawPointer) {
+        if let error = error {
+            print("Save error: \(error.localizedDescription)")
+        } else {
+            print("Depth image saved!")
+        }
     }
 }
 
